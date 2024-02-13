5e65fcffd19085cbaf19366f7464b30377b5d932
shrava-1122 <shravankott11@gmail.com>
2024-02-13 15:54:19 +0530

    update code

--- app/Http/Controllers/EpisodeController.php
+++ app/Http/Controllers/EpisodeController.php
@@ -12,8 +12,6 @@ use App\Models\Season;
 use App\Models\Episode;
 use App\Models\EpisodeArtist;
 
-use Illuminate\Support\Facades\DB;
-
 class EpisodeController extends Controller
 {
     public function index(Request $request)
@@ -22,15 +20,12 @@ class EpisodeController extends Controller
         $seasonid = $request->session()->get('seasonid');
         $series = Series::findOrFail($webid);
         $themeName = $series->theme->title;
-
-
         $webArtists = WebArtist::where('web_id', $webid)->get();
         $seasonArtists = SeasonArtist::where('season_id', $seasonid)->get();
-
-        $webarts=Series::with(['artists'])->where('id', $webid)->get();
-        $seasonarts=Season::with(['artists'])->where('id', $seasonid)->get();
+        $webarts = Series::with(['artists'])->where('id', $webid)->get();
+        $seasonarts = Season::with(['artists'])->where('id', $seasonid)->get();
         $posts = Episode::where('season_id', $seasonid)->get();
-        return view('Episode.episodelist', compact('posts', 'themeName', 'webArtists', 'seasonArtists','seasonarts','webarts'));
+        return view('Episode.episodelist', compact('posts', 'themeName', 'webArtists', 'seasonArtists', 'seasonarts', 'webarts'));
     }
     public function add()
     {
@@ -38,17 +33,13 @@ class EpisodeController extends Controller
         $web_id = session()->get('webid');
         $webSeries = Series::where('id', $web_id)->where('active', 1)->first();
         $season = Season::where('id', $season_id)->first();
-        if($webSeries&&$season){
-        $selectedArtistIds1 = SeasonArtist::where('season_id', '=', $season_id)->pluck('artist_id')->toArray();
-       
-        $selectedArtistIds2 = WebArtist::where('web_id', '=', $web_id)->pluck('artist_id')->toArray();
-        $selectedArtistIds = array_merge($selectedArtistIds1, $selectedArtistIds2);
-        $artists = Artist::whereNotIn('id', $selectedArtistIds)->pluck('name', 'id');
-    
-        return view('Episode/addepisode', compact('artists'));
-        }
-        else {
-        
+        if ($webSeries && $season) {
+            $selectedArtistIds1 = SeasonArtist::where('season_id', '=', $season_id)->pluck('artist_id')->toArray();
+            $selectedArtistIds2 = WebArtist::where('web_id', '=', $web_id)->pluck('artist_id')->toArray();
+            $selectedArtistIds = array_merge($selectedArtistIds1, $selectedArtistIds2);
+            $artists = Artist::whereNotIn('id', $selectedArtistIds)->pluck('name', 'id');
+            return view('Episode/addepisode', compact('artists'));
+        } else {
             return redirect()->route('web.list')->with('error', 'Web series or season not found.');
         }
     }
@@ -61,18 +52,14 @@ class EpisodeController extends Controller
             'artist_ids' => 'required|array',
             'artist_ids.*' => 'exists:artists,id',
         ]);
-      
         $webSeries = new Episode();
-       
         $webSeries->episode_title = $validatedData['title'];
         $webSeries->description = $validatedData['description'];
         $seasonid = session()->get('seasonid');
-       
         $webSeries->season_id = $seasonid;
         $id = $request->session()->get('id');
         $webSeries->updated_by = $id;
         $webSeries->created_by = $id;
-       
         $webSeries->save();
         foreach ($validatedData['artist_ids'] as $artistId) {
             $seasonArtist = new EpisodeArtist();
@@ -89,18 +76,15 @@ class EpisodeController extends Controller
         $webSeries = Series::where('id', $web_id)->where('active', 1)->first();
         $season = Season::where('id', $season_id)->first();
         $episode = Episode::where('id', $id)->first();
-        if($webSeries&&$season&&$episode){
-        $episode = Episode::findOrFail($id);
-        $artists = Artist::pluck('name', 'id') ?? [];
-        $selectedArtistIds = EpisodeArtist::where('episode_id', '=', $id)->pluck('artist_id')->toArray();
-        return view('Episode/editepisode', compact('episode', 'artists', 'selectedArtistIds'));
-        }
-        else {
-        
+        if ($webSeries && $season && $episode) {
+            $episode = Episode::findOrFail($id);
+            $artists = Artist::pluck('name', 'id') ?? [];
+            $selectedArtistIds = EpisodeArtist::where('episode_id', '=', $id)->pluck('artist_id')->toArray();
+            return view('Episode/editepisode', compact('episode', 'artists', 'selectedArtistIds'));
+        } else {
             return redirect()->route('web.list')->with('error', 'Web series or season or episode not found.');
         }
     }
-
     public function update(Request $request, $id)
     {
         $validatedData = $request->validate([
@@ -125,14 +109,12 @@ class EpisodeController extends Controller
         $web_id = session()->get('webid');
         $webSeries = Series::where('id', $web_id)->where('active', 1)->first();
         $season = Season::where('id', $season_id)->first();
-        if($webSeries&&$season){
-        $episode = Episode::findOrFail($id);
-        $episode->artists()->detach();
-        $episode->delete();
-        return redirect()->route('episode.list')->with('success', 'Web series deleted successfully.');
-        }
-        else {
-        
+        if ($webSeries && $season) {
+            $episode = Episode::findOrFail($id);
+            $episode->artists()->detach();
+            $episode->delete();
+            return redirect()->route('episode.list')->with('success', 'Web series deleted successfully.');
+        } else {
             return redirect()->route('web.list')->with('error', 'Web series or season not found.');
         }
     }
@@ -143,16 +125,13 @@ class EpisodeController extends Controller
         $webSeries = Series::where('id', $webid)->where('active', 1)->first();
         $season = Season::where('id', $seasonid)->first();
         $episode = Episode::where('id', $id)->first();
-        if($webSeries&&$season&&$episode){
-        $webSeries = Series::findOrFail($webid);
-        $season = Season::findOrFail($seasonid);
-        $episode = Episode::findOrFail($id);
-        return view('Episode/episodeview', compact('webSeries', 'season', 'episode'));
-        }
-        else {
-        
+        if ($webSeries && $season && $episode) {
+            $webSeries = Series::findOrFail($webid);
+            $season = Season::findOrFail($seasonid);
+            $episode = Episode::findOrFail($id);
+            return view('Episode/episodeview', compact('webSeries', 'season', 'episode'));
+        } else {
             return redirect()->route('web.list')->with('error', 'Web series or season or Episode not found, try again.');
         }
-        
     }
 }
